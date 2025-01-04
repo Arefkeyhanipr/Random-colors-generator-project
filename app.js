2514a990c3bce07557366f8a1afd9f0ccd657bbc
Aref <Arefkeyhani.pr@gmail.com>
2025-01-04 23:57:07 +0330

    coloring our inputs

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ function randomColors() {
     hexText.innerText = randomColor;
     //Check for contrast
     checkTextContrast(randomColor, hexText);
+    //Initial colorize Sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
 
@@ -32,6 +39,24 @@ function checkTextContrast(color, text) {
   } else {
     text.style.color = "white";
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  //Scale saturation
+  const noSaturation = color.set("hsl.s", 0);
+  const fullSaturation = color.set("hsl.s", 1);
+  const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation]);
+  //Scale Brightness
+  const midBrightness = color.set("hsl.l", 0.5);
+  const scaleBrightness = chroma.scale(["black", midBrightness, "white"]);
+  //Update Input Colors
+  saturation.style.backgroundImage = `linear-gradient(to right,
+  ${scaleSaturation(0)},${scaleSaturation(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBrightness(
+    0
+  )},
+  ${scaleBrightness(0.5)},${scaleBrightness(1)})`;
+  hue.style.backgroundImage = `linear-gradient(to right,rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
 }
 
 randomColors();
